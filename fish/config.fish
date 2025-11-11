aeb0a7f5b93875904ca99a9dbfb70d40dc6c7c1e
Hans Ellegård <psyill.net@gmail.com>
2025-11-11 20:43:54 +0100

    Move graphic related variables to Sway script
    Also add Pipewire and Pulse fixes for non-systemd systems

--- fish/config.fish
+++ fish/config.fish
@@ -2,17 +2,10 @@
 
 fish_add_path --path "$HOME/.local/bin"
 set --export --path RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
+set --export PIPEWIRE_RUNTIME_DIR $XDG_RUNTIME_DIR/pipewire
+set --export PULSE_RUNTIME_PATH $XDG_RUNTIME_DIR/pulse
 
-set --export QT_QPA_PLATFORM wayland-egl
-set --export QT_QPA_PLATFORMTHEME qt5ct
-set --export QT_WAYLAND_DISABLE_WINDOWDECORATION 1
-set --erase QT_STYLE_OVERRIDE
-
-set --export SDL_VIDEODRIVER wayland
-
-set --export REPO_URL file:///var/code/git-repo-bare
-
-#set --export ANDROID_SDK_ROOT "$HOME/android-sdk"
+set --export ANDROID_SDK_ROOT "$HOME/android-sdk"
 #set --export STUDIO_JDK /usr/lib/jvm/java-11-openjdk
 
 if status is-login
